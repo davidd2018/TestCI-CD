169e2d0fb419605a8f0e78bc6bc6921579debf18
davidd2018 <phamtranquang12@gmail.com>
2026-03-08 14:06:11 +0700

    test failed

--- test/sum.py
+++ test/sum.py
@@ -1,6 +1,2 @@
 def sum(a,b):
     return a + b
-
-sum(5, 3)
-
-print(sum(5, 3))
--- test/test_sum.py
+++ test/test_sum.py
@@ -2,9 +2,12 @@ from sum import sum
     
 
 def test_sum_basic():
-    return sum(5, 3) == 8
+    return sum() == 8
+
 
 if(test_sum_basic() == True):
     print("Test passed")
+elif(test_sum_basic() == False):
+    print("Test failed")
 else:
-    print("fail")
+    print("Test error")
