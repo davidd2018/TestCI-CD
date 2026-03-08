f1ce61b0f718f9f022d9ed31c715855d9323aac5
davidd2018 <phamtranquang12@gmail.com>
2026-03-08 13:46:45 +0700

    Testing sum

--- test/test_sum.py
+++ test/test_sum.py
@@ -1,6 +1,6 @@
 from sum import sum
-
+    
 def test_sum_basic():
-    assert sum(5, 3) == 8
-    assert sum(0, 0) == 0
-    assert sum(-1, 1) == 0
+    return sum(5, 2) == 8
+
+print(test_sum_basic())
