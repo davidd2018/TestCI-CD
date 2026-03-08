e823b06caac2f66b04eb2940aa14911518d163f7
davidd2018 <phamtranquang12@gmail.com>
2026-03-08 14:01:08 +0700

    Testing sum

--- test/test_sum.py
+++ test/test_sum.py
@@ -1,6 +1,10 @@
 from sum import sum
     
-def test_sum_basic():
-    return sum(5, 2) == 8
 
-print(test_sum_basic())
+def test_sum_basic():
+    return sum(5, 3) == 8
+
+if(test_sum_basic() == True):
+    print("Test passed")
+else:
+    print("fail")
